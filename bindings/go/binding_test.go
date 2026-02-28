08ee48e09d06da7531b5ab0f74f1941b07866d03
Viruthagiri Thirumavalavan <giridharanbtech@gmail.com>
2026-03-01 05:22:27 +0530

    feat: go bindings

---
+++ bindings/go/binding_test.go
@@ -0,0 +1,15 @@
+package tree_sitter_clojure_test
+
+import (
+	"testing"
+
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_clojure "github.com/tree-sitter/tree-sitter-clojure/bindings/go"
+)
+
+func TestCanLoadGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_clojure.Language())
+	if language == nil {
+		t.Errorf("Error loading clojure grammar")
+	}
+}
